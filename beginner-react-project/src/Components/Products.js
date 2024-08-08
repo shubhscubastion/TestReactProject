85f7eb00da1ad8e6a62ec6003f806077d8e5790c
shubhscubastion <shubh.sarpal@cubastion.com>
2024-08-08 14:36:14 +0530

    Added Parameter and props

--- beginner-react-project/src/Components/Products.js
+++ beginner-react-project/src/Components/Products.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 let productCount=0;
-let isAvailable = "Available"
 
-let badgeClass = 'badge-padding-240 badge '
-
-badgeClass += isAvailable === 'Available' ? 'bg-success' : 'bg-danger'
-function FormattedCountofProduct(){
-
-   return productCount > 0 ? productCount : "Zero"
-    
+function FormattedCountofProduct(){    
+    return productCount > 0 ? productCount : "Zero"
 }
 
 let style = {
@@ -17,21 +11,23 @@ let style = {
     fontSize:14
 }
 
-function Products(){
+function Products(props){
+    let badgeClass = 'badge-padding-240 badge '
+    badgeClass += props.available ? 'bg-success' : 'bg-danger'
     return (<ul className="list-group shadow">
                 <li className="list-group-item">
                     <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
-                        <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-                        <p className="font-italic text-muted mb-0 small">A short description about the Product</p>
+                        <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                        <p className="font-italic text-muted mb-0 small">{props.description}</p>
                         <div className="d-flex align-items-center justify-content-start mt-1">
-                            <h6 className="font-weight-bold my-2" style={{'margin-right':30}}>$120.00</h6>
+                            <h6 className="font-weight-bold my-2" style={{'margin-right':30}}>${props.price}</h6>
                             <button className="btn btn-primary">-</button>
                             <span style={style}>{FormattedCountofProduct()}</span>
                             <button className="btn btn-primary">+</button>
-                            <span className={badgeClass}>{isAvailable}</span>
+                            <span className={badgeClass}>{props.available ? 'Available':'Unavailable'}</span>
                         </div>
-                        </div> <img src={require ("../Images/milk.png")} alt ="Generic placeholder image" width="100" className="ml-lg-5 order-1 order-lg-2" style={{'margin-left':100}}/>
+                        </div> <img src={props.image} alt ="Generic placeholder image" width="100" className="ml-lg-5 order-1 order-lg-2" style={{'margin-left':100}}/>
                     </div>
                 </li>
     </ul>)
