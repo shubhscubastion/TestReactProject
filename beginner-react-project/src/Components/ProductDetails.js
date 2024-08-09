cc3f5682622fa04968dd77e546974ed1348f0aaa
shubhscubastion <shubh.sarpal@cubastion.com>
2024-08-09 15:39:31 +0530

    Added the functionality of Buttons

--- beginner-react-project/src/Components/ProductDetails.js
+++ beginner-react-project/src/Components/ProductDetails.js
@@ -1,12 +1,20 @@
 import Button from "./Buttons";
-
-let productCount=0;
-
-function FormattedCountofProduct(){    
-    return productCount > 0 ? productCount : "Zero"
-}
-
+import { useState } from 'react';
 function ProductDetails(props){
+    let [productCount,setCount]=useState(0);
+    
+    function FormattedCountofProduct(){    
+        return (productCount > 0 ? productCount : "Zero"
+        )
+    }
+    let IncrementNumber =function (){
+        setCount(++productCount)
+        
+    }
+    let DecrementNumber =function (){
+        setCount(--productCount);
+        
+    }
     let badgeClass = 'badge-padding-240 badge '
     badgeClass += props.available ? 'bg-success' : 'bg-danger'
     let style = {
@@ -16,9 +24,9 @@ function ProductDetails(props){
         return(
             <div className="d-flex align-items-center justify-content-start mt-1">
                                     <h6 className="font-weight-bold my-2" style={{'margin-right':30}}>${props.price}</h6>
-                                    <Button>-</Button>
+                                    <Button eventHandler={DecrementNumber}>-</Button>
                                     <span style={style}>{FormattedCountofProduct()}</span>
-                                    <Button>+</Button>
+                                    <Button eventHandler={IncrementNumber}>+</Button>
                                     <span className={badgeClass}>{props.available ? 'Available':'Unavailable'}</span>
                                 </div>
         );
